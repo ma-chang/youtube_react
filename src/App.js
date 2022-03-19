c334b83bab2f71020f670938fd6c236827332657
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-20 08:44:34 +0900

    setting mui theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import indigo from '@material-ui/core/colors/indigo';
+import createTheme from '@material-ui/core/styles/createTheme';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: {
+      main: '#f44336',
+    },
+  },
+  typography: {
+    fontFamily: 'Comic Neue',
+  },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MuiThemeProvider theme={theme}></MuiThemeProvider>;
 }
 
 export default App;
